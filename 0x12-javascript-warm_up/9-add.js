7be6e70b8f05120ad4f3644c41614dd0cae9c506
olaoluwa omoduyilemi <88881222+duyilemiola@users.noreply.github.com>
2023-04-12 23:44:33 +0100

    Update 9-add.js

--- 0x12-javascript-warm_up/9-add.js
+++ 0x12-javascript-warm_up/9-add.js
@@ -1,14 +1,6 @@
 #!/usr/bin/node
-
 function add (a, b) {
   return a + b;
 }
 
-let a = parseInt(process.argv[2]);
-let b = parseInt(process.argv[3]);
-
-if (a && b) {
-  console.log(add(a, b));
-} else {
-  console.log(b);
-}
+console.log(add(Number(process.argv[2]), Number(process.argv[3])));
