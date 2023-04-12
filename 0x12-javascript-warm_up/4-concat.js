1015b5e4ce90aaf499871af3a796272ae89cfb01
olaoluwa omoduyilemi <88881222+duyilemiola@users.noreply.github.com>
2023-04-12 23:41:11 +0100

    Update 4-concat.js

--- 0x12-javascript-warm_up/4-concat.js
+++ 0x12-javascript-warm_up/4-concat.js
@@ -1,15 +1,2 @@
 #!/usr/bin/node
-
-let firstArg = process.argv[2];
-let is = ' is ';
-let secondArg = process.argv[3];
-
-if (firstArg === undefined) {
-  firstArg = 'undefined';
-}
-if (secondArg === undefined) {
-  secondArg = 'undefined';
-}
-
-let printString = firstArg.concat(is).concat(secondArg);
-console.log(printString);
+console.log(`${process.argv[2]} is ${process.argv[3]}`);
